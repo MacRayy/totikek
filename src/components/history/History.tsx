9b80506a146e6ccbcf069f4c66c62a52149a79f5
Zsolt Makrai <mecrai@gmail.com>
2025-11-11 21:48:24 +0100

    feat: add history content

--- src/components/history/History.tsx
+++ src/components/history/History.tsx
@@ -1,7 +1,62 @@
-import { EmptyPage } from '@/ui-components/empty-page/EmptyPage'
+'use client'
+import { H1, H2, Body } from '@/styles/Typography'
+import * as Styled from './History.styles'
 
 export const History = () => {
   return (
-    <EmptyPage description="A Káptalantóti történetéröl szóló oldal jelenleg fejlesztés alatt áll" />
+    <Styled.Section>
+      <H1 align="center">Káptalantóti története</H1>
+      <Styled.Article>
+        <H2>1. fejezet – Fülöp a vadgesztenyefák alatt</H2>
+        <Body>Szia! Fülöp vagyok, a füleskuvik.</Body>
+        <Body>
+          Történetem a falu közepén, a Szent Márton püspök-templom előtt kezdődik. A templom mellett
+          csendesen áll a Szentháromság-szobor, körülötte pedig öreg, terebélyes vadgesztenyefák
+          hajlanak a tér fölé. Amikor itt ülök az ágakon, mintha a múlt mesélne nekem.
+        </Body>
+        <Body>
+          Éppen egy ilyen estén motozást hallottam odalent, a vadgesztenyék gyökerei között. – Félre
+          minden tüskés kupacs! Megérkeztem! – dörmögte jókedvűen Borz Bendegúz, aki mindig úgy tűnt
+          fel, mintha a falu összes levele ráragadt volna.
+        </Body>
+        <Body>
+          – Szervusz, Bendegúz! – köszöntöttem. – Úgy látom, megint történelemkutatáson vagy. – Hát
+          hogyne lennék! – pöfögte büszkén. – Képzeld, a falut már 1256-ban „Tothy” néven
+          említették, és akkor még egészen másként nézett ki a környék. Később a győri káptalan is
+          birtokolt itt földeket, így került a név elé a „Káptalan”.
+        </Body>
+        <Body>
+          Lent a téren nesztelenül libbent végig a szél. A templom falai tompán visszhangozták
+          Bendegúz hangját. – Régen két Tóti volt – tettem hozzá. – Alsótóti és Felsőtóti. – Bizony
+          – bólogatott a borz. – A két település csak 1848-ban egyesült, abból lett a mai
+          Káptalantóti. Nekem tetszik ez a történet… olyan, mintha két falu összefogott volna, hogy
+          erősebbek legyenek.
+        </Body>
+        <Body>
+          A Szentháromság-szobor kövei halványan csillogtak a templom ablakaiból kiszűrődő fényben.
+          A tér békés volt, a vadgesztenyék koronája óriásként zárta körbe a helyet. – Szeretem,
+          ahogy ez a templom itt áll – mondta Bendegúz. – Már 1272-ben megemlítették, a mai alakját
+          pedig 1773-ban kapta. Milyen sok mindent látott azóta!
+        </Body>
+        <Body>
+          – Sokszor hallgatom innen a harangját – feleltem. – Olyan szépen száll végig a
+          Tapolcai-medencén, hogy még a hegyek is visszamondják. Bendegúz elmerengett, ahogy a tér
+          peremén túl a dombok sötét körvonala kirajzolódott. – Csoda ez a vidék – mormogta. – Hét
+          hegy öleli körül: a Tóti-hegy, a Gulács, a Szent György-hegy, a Csobánc, a Hajagos, a Bács
+          és a Sabar. Nem véletlen, hogy a szőlők itt ennyire szeretik a talajt. A bazaltos föld és
+          a napsütötte lejtők szinte hívogatják őket.
+        </Body>
+        <Body>
+          – A Tóti-hegy a kedvencem – tettem hozzá. – Csak 346 méter magas, mégis innen látni a
+          legszebb körpanorámát a környéken. A borz halkan hümmögött. – Egyszer felmegyünk együtt…
+          vagy legalábbis te repülsz, én meg lihegek mögötted – mondta, amitől hangosan felnevettem.
+        </Body>
+        <Body>
+          A vadgesztenyék koronája lassan összezárult felettünk, mintha kicsit jobban őriznék a
+          történetet. – Ha szeretnéd, folytatom a mesémet a következő táblánál is. Kövesd a kék
+          madártollakat, azok mutatják az utat.
+        </Body>
+      </Styled.Article>
+    </Styled.Section>
   )
 }
